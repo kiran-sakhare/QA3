5a2c266dde770e78a7393672c3e62befea0ce233
kiran-sakhare <kmeshram567@gmail.com>
2025-03-15 16:46:56 +0530

    Add files via upload

---
+++ QN3.py
@@ -0,0 +1,12 @@
+score=int(input("enter the score:"))
+if score>=90 or score<=100:
+    print("grade:""A")
+elif score>=80 or score<90:
+    print("grade:""B")
+elif score>=70 or score<80:
+    print("grade:""c")
+elif score>=60 or score<70:
+    print("grade:""D")
+else:
+    print("grade:""F")
+
